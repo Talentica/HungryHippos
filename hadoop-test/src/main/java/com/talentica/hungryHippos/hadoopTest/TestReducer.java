8c2c087cc179800d136bb7e12c044bf7b9fa0625
Debasish Ray Chawdhuri <debasish.chawdhuri@talentica.com>
2015-09-28 16:47:19 +0530

    testing

--- hadoop-test/src/main/java/com/talentica/hungryHippos/hadoopTest/TestReducer.java
+++ hadoop-test/src/main/java/com/talentica/hungryHippos/hadoopTest/TestReducer.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 /**
  * Created by debasishc on 24/9/15.
  */
-public class TestReducer extends Reducer<Text, Text, Text, DoubleWritable> {
+public class TestReducer extends Reducer<Text, Text, Text, Text> {
     DescriptiveStatistics descriptiveStatistics1 = new DescriptiveStatistics();
     DescriptiveStatistics descriptiveStatistics2 = new DescriptiveStatistics();
     @Override
@@ -21,8 +21,8 @@ public class TestReducer extends Reducer<Text, Text, Text, DoubleWritable> {
             descriptiveStatistics1.addValue(Double.parseDouble(parts[0]));
             descriptiveStatistics2.addValue(Double.parseDouble(parts[1]));
         }
-        DoubleWritable v1 = new DoubleWritable(descriptiveStatistics1.getPercentile(50));
-        DoubleWritable v2 = new DoubleWritable(descriptiveStatistics2.getPercentile(50));
+        Text v1 = new Text(""+descriptiveStatistics1.getPercentile(50));
+        Text v2 = new Text(""+descriptiveStatistics2.getPercentile(50));
         context.write(key, v1);
         context.write(key, v2);
         descriptiveStatistics1.clear();

7a58fe9076c26182f0d07b7b20ff65d634dd2dde
Nitin Kasat <nitin.kasat@talentica.com>
2016-02-05 12:01:52 +0530

    Fixing issue in cleanup script to clean up bucketCombination map file. {r:pooshans}

--- master/scripts/cleanup-manager.sh
+++ master/scripts/cleanup-manager.sh
@@ -6,5 +6,5 @@ node_pwd=`cat ./node_pwd_file.txt|grep "pwd"|awk -F":" '{print $2}'`
 for node in `echo $manager_node_ip`
 do
    echo "Cleaning HungryHippos node $node"
-   sshpass -p $node_pwd ssh root@$node "cd hungryhippos/manager;rm Application.log;rm bucketToNodeNumberMap;rm bucketToNodeNumbersMap;rm keyToValueToBucketMap"
+   sshpass -p $node_pwd ssh root@$node "cd hungryhippos/manager;rm Application.log*;rm bucketToNodeNumberMap;rm bucketCombinationToNodeNumbersMap;rm keyToValueToBucketMap"
 done

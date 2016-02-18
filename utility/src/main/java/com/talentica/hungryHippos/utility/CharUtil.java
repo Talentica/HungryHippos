3d999ef832b5926a371ea5bb2c0186bd1c545c83
Pooshan <Pooshan@viewics.com>
2016-02-18 15:37:50 +0530

    Char Utility to optimize the memory consumption used by Character object. And few comments are modified.

--- utility/src/main/java/com/talentica/hungryHippos/utility/CharUtil.java
+++ utility/src/main/java/com/talentica/hungryHippos/utility/CharUtil.java
@@ -22,9 +22,9 @@ public class CharUtil {
 	 * It return the resultant value corresponding to the all ASCII value positioning each ASCII values
 	 * to bit's position from right 
 	 * 
-	 * @param ch
-	 * @param bufferSize
-	 * @return int
+	 * @param ch char arrays
+	 * @param bufferSize 32 bits
+	 * @return int value
 	 */
 	public static int getIntValue(char[] ch){
 		if(ch.length > totalByte){
@@ -47,14 +47,15 @@ public class CharUtil {
 	 * It returns the all char in arrays corresponding to the bits representation of the given numbers.
 	 * 
 	 * @param value
-	 * @param bufferSize
-	 * @return char[]
+	 * @param bufferSize 32 bits
+	 * @return char[] arrays of chars
 	 */
 	public static char[] getCharArrays(int value){
 		int bitsPerByte = 8;
 		int oldValue = value;
 		int noOfBytes = bufferSize / bitsPerByte ;
 		char[] chs = new char[noOfBytes] ;
+		
 		for(int i = (noOfBytes-1) ; i >= 0  ; i--){
 			int tempValue = oldValue;
 			int newValue = (tempValue >> (bitsPerByte * i)) & 0x000000FF;
--- utility/src/test/java/com/talentica/hungryHippos/utility/CharUtilTest.java
+++ utility/src/test/java/com/talentica/hungryHippos/utility/CharUtilTest.java
@@ -13,20 +13,25 @@ import org.junit.Test;
  */
 public class CharUtilTest {
 	
-	private static String expected = "abcd";
-	char[] chrs;
+	private static String expected1 = "ab";
+	private static String expected2 = "zxrf";
+	char[] chrs1;
+	char[] chrs2;
 	int bufferSize;
 	
 	
 	@Before
 	public void setUp(){
-		chrs = "abcd".toCharArray();
+		chrs1 = "ab".toCharArray();
+		chrs2 = "zxrf".toCharArray();
 	}
 	
 	@Test
 	public void testEqualChars(){
-		int value = CharUtil.getIntValue(chrs);
-		Assert.assertEquals(expected, new String(CharUtil.getCharArrays(value)));
+		int value1 = CharUtil.getIntValue(chrs1);
+		Assert.assertEquals(expected1, new String(CharUtil.getCharArrays(value1)).trim());
+		int value2 = CharUtil.getIntValue(chrs2);
+		Assert.assertEquals(expected2, new String(CharUtil.getCharArrays(value2)));
 	}
 
 }

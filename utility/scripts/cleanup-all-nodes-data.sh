d0f69f62c69608de26c23c67585203102ec98581
Nitin Kasat <nitin.kasat@talentica.com>
2016-02-26 18:13:24 +0530

    Fixing node jar build failure.

--- utility/scripts/cleanup-all-nodes-data.sh
+++ utility/scripts/cleanup-all-nodes-data.sh
@@ -6,5 +6,5 @@ cat ./../../utility/src/main/resources/serverConfigFile.properties|awk -F":" '{p
 for node in `cat node_ips_list.txt`
 do
    echo "Cleaning up HungryHippos node  $node"
-   ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos;rm ./data/Application.log*;rm Application.log*;rm outputFile;"
+   ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos;rm ./data/Application.log*;rm Application.log*;rm system.out;rm system.err;rm outputFile;"
 done
--- utility/scripts/cleanup-sharding.sh
+++ utility/scripts/cleanup-sharding.sh
@@ -6,5 +6,5 @@ sharding_node_ip=`cat ./node_pwd_file.txt|grep "sharding_node_ip"|awk -F":" '{pr
 for node in `echo $sharding_node_ip`
 do
    echo "Cleaning up sharding node: $node"
-   ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos/sharding;rm sharding.log*;rm sharding.log*;"
+   ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos/sharding;rm sharding.log*;rm sharding.log*;rm system.out;rm system.err"
 done

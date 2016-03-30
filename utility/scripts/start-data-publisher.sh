8e5c366f46512a3973a1b387f475820ecb199bf0
Pooshan <pooshan.singh@talentica.com>
2016-03-30 17:07:24 +0530

    chnages : copy the sharded file in script.

--- utility/scripts/start-data-publisher.sh
+++ utility/scripts/start-data-publisher.sh
@@ -5,7 +5,7 @@ sh start-zk-server.sh
 
 for node in `cat data_publisher_node_ips.txt`
 do
-   scp root@$node:hungryhippos/sharding/*Map root@$node:hungryhippos/data-publisher/
+    ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos/sharding;sh copy-shard-files-to-all-nodes.sh"
    echo "Starting data publisher $node"
    ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos/data-publisher;java -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -cp data-publisher.jar:test-jobs.jar com.talentica.hungryHippos.master.DataPublisherStarter ./config.properties > ./system.out 2>./system.err &"
 done

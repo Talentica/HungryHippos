0b70ea62e554a2d415d9f0b6d5d5df37f4efceab
Pooshan <pooshan.singh@talentica.com>
2016-04-05 19:01:10 +0530

    Auto creation of the digital ocean server and start of zookeeper

---
+++ utility/scripts/create_proj_directory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /root/hungryhippos/
+echo "Creating tmp directory"
+rm -r tmp
+mkdir tmp
+echo "temp directory created"
+echo "Copying files"
+cp /root/hungryhippos/checkout/HungryHippos/utility/scripts/*zk-server* /root/hungryhippos/tmp/
+cp /root/hungryhippos/checkout/HungryHippos/digital-ocean-manager/build/libs/digital-ocean.jar /root/hungryhippos/tmp/
+cp /root/hungryhippos/checkout/HungryHippos/digital-ocean-manager/src/main/resources/json/* /root/hungryhippos/tmp/
+echo "Files are copied."

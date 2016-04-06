1c6d2ac7bdcd5547ed96374b9c45afaf2497ac02
Pooshan <pooshan.singh@talentica.com>
2016-04-06 19:13:09 +0530

    1. Start Zookeeper after droplet creation. 2. Minor changes in script file. 3. Moved some java file from utility module to coordination module.

--- utility/scripts/create_proj_directory.sh
+++ utility/scripts/create_proj_directory.sh
@@ -9,4 +9,5 @@ cp /root/hungryhippos/checkout/HungryHippos/utility/scripts/*zk-server* /root/hu
 cp /root/hungryhippos/checkout/HungryHippos/digital-ocean-manager/build/libs/digital-ocean.jar /root/hungryhippos/tmp/
 cp /root/hungryhippos/checkout/HungryHippos/digital-ocean-manager/src/main/resources/json/* /root/hungryhippos/tmp/
 cp /root/hungryhippos/checkout/HungryHippos/utility/src/main/resources/config.properties /root/hungryhippos/tmp/
+chmod -R 777 /root/hungryhippos/tmp/
 echo "Files are copied."

37cb66b72f03f065c9788d607e4f199b15670720
Pooshan <pooshan.singh@talentica.com>
2016-04-11 20:42:23 +0530

    Notification for different activity is implemented.

--- utility/scripts/start-job-manager.sh
+++ utility/scripts/start-job-manager.sh
@@ -8,3 +8,5 @@ do
    echo "Starting job manager on $node"
    ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos/job-manager;java -XX:HeapDumpPath=./ -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -cp job-manager.jar:test-jobs.jar com.talentica.hungryHippos.job.main.JobManagerStarter $1 > ./system.out 2>./system.err &"
 done
+
+start-all-nodes-job-execution.sh

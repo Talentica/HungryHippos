b7e290e4f27b1edc07be7c5972bb1b77dc682cfe
Pooshan <pooshan.singh@talentica.com>
2016-04-12 20:20:30 +0530

    script change

--- utility/scripts/remove-ssh-keygen.sh
+++ utility/scripts/remove-ssh-keygen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cat /root/hungryhippos/tmp/serverConfigFile.properties|awk -F":" '{print $2}' > node_ips_list.txt
-
-for node in `cat node_ips_list.txt`
+cat /root/hungryhippos/tmp/serverConfigFile.properties|awk -F":" '{print $2}' > node_ips_list_for_keygen.txt
+cat /root/hungryhippos/tmp/output_ip_file >> node_ips_list_for_keygen.txt
+for node in `cat node_ips_list_for_keygen.txt`
 do
    echo "Removing ssh keygen for $node"
    ssh-keygen -f "/root/.ssh/known_hosts" -R $node

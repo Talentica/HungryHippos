d981de23917ca6a651d3adde2145064270dbfa10
Pooshan <pooshan.singh@talentica.com>
2016-04-12 10:36:52 +0530

    script changes

--- utility/scripts/copy-shard-files-to-all-nodes.sh
+++ utility/scripts/copy-shard-files-to-all-nodes.sh
@@ -7,21 +7,21 @@ job_manager_ip=`cat /root/hungryhippos/tmp/master_ip_file`
 for node in `cat node_ips_list.txt`
 do
    echo "Copying file to $node"
-   scp -o "StrictHostKeyChecking no" root@$job_manager_ip:hungryhippos/sharding/keyToValueToBucketMap root@$node:hungryhippos
-   scp -o "StrictHostKeyChecking no" root@$job_manager_ip:hungryhippos/sharding/bucketToNodeNumberMap root@$node:hungryhippos
-   scp -o "StrictHostKeyChecking no" root@$job_manager_ip:hungryhippos/sharding/bucketCombinationToNodeNumbersMap root@$node:hungryhippos
+   ssh -o "StrictHostKeyChecking no" root@$node `ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""`
+   scp -o "StrictHostKeyChecking no" /root/hungryhippos/sharding/keyToValueToBucketMap root@$node:hungryhippos
+   scp -o "StrictHostKeyChecking no" /root/hungryhippos/sharding/bucketToNodeNumberMap root@$node:hungryhippos
+   scp -o "StrictHostKeyChecking no" /root/hungryhippos/sharding/bucketCombinationToNodeNumbersMap root@$node:hungryhippos
 done
 
 for data_publisher_node_ip in `cat data_publisher_node_ips.txt`
 do
 echo 'copying files on data publisher'
-   scp -o "StrictHostKeyChecking no" root@$job_manager_ip:hungryhippos/sharding/bucketCombinationToNodeNumbersMap root@$data_publisher_node_ip:hungryhippos/data-publisher
-   scp -o "StrictHostKeyChecking no" root@$job_manager_ip:hungryhippos/sharding/keyToValueToBucketMap root@$data_publisher_node_ip:hungryhippos/data-publisher
-   scp -o "StrictHostKeyChecking no" root@$job_manager_ip:hungryhippos/sharding/bucketToNodeNumberMap root@$data_publisher_node_ip:hungryhippos/data-publisher
+   scp -o "StrictHostKeyChecking no" /root/hungryhippos/sharding/bucketCombinationToNodeNumbersMap /root/hungryhippos/data-publisher/
+   scp -o "StrictHostKeyChecking no" /root/hungryhippos/sharding/keyToValueToBucketMap /root/hungryhippos/data-publisher/
+   scp -o "StrictHostKeyChecking no" /root/hungryhippos/sharding/bucketToNodeNumberMap /root/hungryhippos/data-publisher/
 done
 
 echo 'copying files on job-manager'
-   scp -o "StrictHostKeyChecking no" root@$job_manager_ip:hungryhippos/sharding/bucketCombinationToNodeNumbersMap root@$job_manager_ip:hungryhippos/job-manager
-   scp -o "StrictHostKeyChecking no" root@$job_manager_ip:hungryhippos/sharding/keyToValueToBucketMap root@$job_manager_ip:hungryhippos/job-manager
-   scp -o "StrictHostKeyChecking no" root@$job_manager_ip:hungryhippos/sharding/bucketToNodeNumberMap root@$job_manager_ip:hungryhippos/job-manager
-
+   scp -o "StrictHostKeyChecking no" /root/hungryhippos/sharding/bucketCombinationToNodeNumbersMap /root/hungryhippos/job-manager/
+   scp -o "StrictHostKeyChecking no" /root/hungryhippos/sharding/keyToValueToBucketMap /root/hungryhippos/job-manager/
+   scp -o "StrictHostKeyChecking no" /root/hungryhippos/sharding/bucketToNodeNumberMap /root/hungryhippos/job-manager/

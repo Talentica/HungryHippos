e8c778ec249a44774540c13ec48de39754df2859
Pooshan <pooshan.singh@talentica.com>
2016-04-14 17:40:33 +0530

    1. script changes. 2. zkStart server path is resolved.

---
+++ utility/scripts/startServer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo '################          Create droplets      ################'
+sh create_droplets.sh
+echo '################          Droplet creation is initiated      ################'
+
+echo '################          Removing older host key      ################'
+ssh-keygen -R 127.0.0.1
+echo '################          Done      ################'
+
+echo '################          Copying master ip file      ################'
+sh copy-files-to-all-nodes.sh
+echo '################          Master ip file is copied      ################'
+
+echo '################          Sharding setup started      ################'
+sh setup-sharding.sh
+echo '################          Sharding setup completed      ################'
+
+echo '################          Data publisher setup started      ################'
+sh setup-data-publisher.sh
+echo '################          Data publisher setup completed      ################'
+
+echo '################          Job manager setup started      ################'
+sh setup-job-manager.sh
+echo '################          Job manager setup completed      ################'
+
+echo '################          Nodes setup started      ################'
+sh setup-nodes.sh
+echo '################          Nodes setup completed      ################'
+
+echo '################          START SHARDING,DATA PUBLISHING AND JOB MATRIX SEQUENCIALLY     ################'
+sh start-sharding-and-datapublishing-and-jobmatrix.sh
+echo '################          PROCESS INITIATED      ################'

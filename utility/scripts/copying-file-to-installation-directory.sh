b9a0a61e698a7d71eccbfd473e779dcb1bf6fd0d
Pooshan <pooshan.singh@talentica.com>
2016-04-18 15:13:06 +0530

    change script file

--- utility/scripts/copying-file-to-installation-directory.sh
+++ utility/scripts/copying-file-to-installation-directory.sh
@@ -6,4 +6,7 @@ cp ../../job-manager/build/libs/*.jar /root/hungryhippos/intallation/HungryHippo
 cp ../../node/build/libs/*.jar /root/hungryhippos/intallation/HungryHippos/lib/
 cp ../../sharding/build/libs/*.jar /root/hungryhippos/intallation/HungryHippos/lib/
 cp ../../test-hungry-hippos/build/libs/*.jar /root/hungryhippos/intallation/HungryHippos/lib/
+cp ../../coordination/build/classes/main/config.properties /root/hungryhippos/intallation/HungryHippos/conf/
+cp ../../digital-ocean-manager/build/classes/main/json/* /root/hungryhippos/intallation/HungryHippos/json/
+cp ../../utility/scripts/*.sh /root/hungryhippos/intallation/HungryHippos/bin/
 echo '####Completed####'

651e96c1ff860905f1833988ed55c6dc749a3f0a
Pooshan <pooshan.singh@talentica.com>
2016-04-25 14:51:29 +0530

    test-job-jar file is placed in jobuuid directory.

--- utility/scripts/setup-job-manager.sh
+++ utility/scripts/setup-job-manager.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+jobUuid=$1
 sh shut-down-job-manager.sh
 sh cleanup-job-manager.sh
 echo 'Copying new build'
 sh copy-file-to-job-manager.sh ../lib/job-manager.jar
 echo 'Copying test jobs jar'
-sh copy-file-to-job-manager.sh ../lib/test-jobs.jar
+sh copy-file-to-job-manager.sh ../lib/$jobUuid/test-jobs.jar
--- utility/scripts/setup-nodes.sh
+++ utility/scripts/setup-nodes.sh
@@ -1,5 +1,6 @@
+jobUuid=$1
 sh shut-down-all-nodes.sh
 sh cleanup-all-nodes-data.sh
-sh copy-test-jobs-jar-all-nodes.sh
+sh copy-test-jobs-jar-all-nodes.sh $jobUuid
 sh copy-node-jar-to-all-nodes.sh
 sh create-node-id-files.sh

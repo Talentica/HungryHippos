e4d3d490df105a0c982af19e2fdb89b67c5d39cb
Nitin Kasat <nitin.kasat@talentica.com>
2016-04-26 18:13:19 +0530

    Adding logging in JobRequest.

--- digital-ocean-manager/src/main/java/com/talentica/hungryHippos/droplet/query/JobRequest.java
+++ digital-ocean-manager/src/main/java/com/talentica/hungryHippos/droplet/query/JobRequest.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.talentica.hungryHippos.coordination.utility.Property;
@@ -20,15 +22,17 @@ import com.talentica.hungryHippos.tester.api.job.JobServiceResponse;
  */
 public class JobRequest {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(JobRequest.class);
+
 	private static final ObjectMapper MAPPER = new ObjectMapper();
 
 	public Job getJobDetails(String uuid) throws HttpException, IOException {
-			final String WEBSERVER_IP = Property.getProperties().getProperty(
-					"common.webserver.ip");
-			final String WEBSERVER_PORT = Property.getProperties().getProperty("common.webserver.port");
-			final String WEBSERVER_IP_PORT = WEBSERVER_IP + ":" + WEBSERVER_PORT;
-		GetMethod getJobDetails = new GetMethod("http://" + WEBSERVER_IP_PORT
-					+ "/job/any/detail/" + uuid);
+		final String WEBSERVER_IP = Property.getProperties().getProperty("common.webserver.ip");
+		final String WEBSERVER_PORT = Property.getProperties().getProperty("common.webserver.port");
+		final String WEBSERVER_IP_PORT = WEBSERVER_IP + ":" + WEBSERVER_PORT;
+		String uri = "http://" + WEBSERVER_IP_PORT + "/job/any/detail/" + uuid;
+		LOGGER.info("Getting details of job:{}", uri);
+		GetMethod getJobDetails = new GetMethod(uri);
 		HttpClient httpClient = new HttpClient();
 		httpClient.executeMethod(getJobDetails);
 		JobServiceResponse jobServiceResponse = MAPPER.readValue(getJobDetails.getResponseBody(),
--- hungryhippos-tester-web/src/main/java/com/talentica/hungryHippos/tester/web/job/data/STATUS.java
+++ hungryhippos-tester-web/src/main/java/com/talentica/hungryHippos/tester/web/job/data/STATUS.java
@@ -1,10 +1,28 @@
 package com.talentica.hungryHippos.tester.web.job.data;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum STATUS {
-	NOT_STARTED, 
-	STARTED, 
-	IN_PROGRESS, 
-	COMPLETED, 
-	ERROR;
+
+	NOT_STARTED("Not Started"), 
+	STARTED("Started"), 
+	IN_PROGRESS("In Progress"), 
+	COMPLETED("Completed"), 
+	ERROR("Error");
 	
+	private String name;
+	
+	private STATUS(String name){
+		this.name=name;
+	}
+	
+	@JsonValue
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String toString(){
+		return name;
+	}
 }

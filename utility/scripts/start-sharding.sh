a6aba67047e2c03a3731de4b8c4af9167b8f5675
Pooshan <pooshan.singh@talentica.com>
2016-04-29 20:35:26 +0530

    bug fixed.

--- utility/scripts/start-sharding.sh
+++ utility/scripts/start-sharding.sh
@@ -6,13 +6,13 @@ sharding_node_ip=`cat ../$jobUuid/master_ip_file`
 for node in `echo $sharding_node_ip`
 do
    echo "Starting process db entry script"
-   ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos/sharding;java -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -cp sharding.jar com.talentica.hungryHippos.sharding.main.StartProcessDBEntryMain $jobUuid > ./system.out 2>./system.err &"
+   ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos/sharding;java -cp sharding.jar com.talentica.hungryHippos.sharding.main.StartProcessDBEntryMain $jobUuid > ./system_db_process_entry.out 2>./system_db_process_entry.err &"
    echo "Started process db entry"
-   
+
    echo "Starting kazoo server"
-   ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos/sharding;java -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -cp sharding.jar com.talentica.hungryHippos.sharding.main.StartKazooScriptMain $jobUuid > ./system.out 2>./system.err &"
+   ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos/sharding;java -cp sharding.jar com.talentica.hungryHippos.sharding.main.StartKazooScriptMain $jobUuid > ./system.out_kazoo_server 2>./system_kazoo_server.err &"
    echo "Kazoo Server started"
-   
+
    echo "Starting sharding on $node"
-   ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos/sharding;java -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -cp sharding.jar com.talentica.hungryHippos.sharding.main.ShardingStarter $jobUuid > ./system.out 2>./system.err &"
-done	
+   ssh -o StrictHostKeyChecking=no root@$node "cd hungryhippos/sharding;java -cp sharding.jar com.talentica.hungryHippos.sharding.main.ShardingStarter $jobUuid > ./sharding_system.out 2>./sharding_system.err &"
+done

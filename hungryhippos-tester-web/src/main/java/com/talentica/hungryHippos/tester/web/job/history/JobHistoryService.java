010bca03c8d23c098fec55a92de253278a39e148
Nitin Kasat <nitin.kasat@talentica.com>
2016-05-02 21:01:06 +0530

    1. Fixed issues with total job execution time not getting shown on UI. 2. Fixed issues with login because of adding default login page.

--- hungryhippos-tester-web/src/main/java/com/talentica/hungryHippos/tester/web/job/history/JobHistoryService.java
+++ hungryhippos-tester-web/src/main/java/com/talentica/hungryHippos/tester/web/job/history/JobHistoryService.java
@@ -1,5 +1,10 @@
 package com.talentica.hungryHippos.tester.web.job.history;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +13,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.talentica.hungryHippos.tester.api.Service;
 import com.talentica.hungryHippos.tester.web.UserCache;
+import com.talentica.hungryHippos.tester.web.job.data.Job;
 import com.talentica.hungryHippos.tester.web.job.data.JobRepository;
 import com.talentica.hungryHippos.tester.web.user.data.User;
 
 @Controller
 @RequestMapping("/job")
 public class JobHistoryService extends Service {
+
+	private static final Mapper DOZER_BEAN_MAPPER = new DozerBeanMapper();
 
 	@Autowired(required = false)
 	private JobRepository jobRepository;
@@ -25,7 +33,13 @@ public class JobHistoryService extends Service {
 	public @ResponseBody JobHistoryServiceResponse getJobsRecentlyExecutedByUser() {
 		User user = userCache.getCurrentLoggedInUser();
 		JobHistoryServiceResponse jobHistoryServiceResponse = new JobHistoryServiceResponse();
-		jobHistoryServiceResponse.setJobs(jobRepository.findTop5ByUserIdOrderByDateTimeSubmittedDesc(user.getUserId()));
+		List<Job> entities = jobRepository.findTop5ByUserIdOrderByDateTimeSubmittedDesc(user.getUserId());
+		List<com.talentica.hungryHippos.tester.api.job.Job> jobs = new ArrayList<>(0);
+		if (entities != null) {
+			entities.forEach(
+					job -> jobs.add(DOZER_BEAN_MAPPER.map(job, com.talentica.hungryHippos.tester.api.job.Job.class)));
+		}
+		jobHistoryServiceResponse.setJobs(jobs);
 		return jobHistoryServiceResponse;
 	}
 
--- hungryhippos-tester-web/src/main/java/com/talentica/hungryHippos/tester/web/job/history/JobHistoryServiceResponse.java
+++ hungryhippos-tester-web/src/main/java/com/talentica/hungryHippos/tester/web/job/history/JobHistoryServiceResponse.java
@@ -3,9 +3,11 @@ package com.talentica.hungryHippos.tester.web.job.history;
 import java.util.List;
 
 import com.talentica.hungryHippos.tester.api.ServiceResponse;
-import com.talentica.hungryHippos.tester.web.job.data.Job;
+import com.talentica.hungryHippos.tester.api.job.Job;
 
 public class JobHistoryServiceResponse extends ServiceResponse {
+
+	private static final long serialVersionUID = 1L;
 
 	private List<Job> jobs;
 
@@ -15,6 +17,9 @@ public class JobHistoryServiceResponse extends ServiceResponse {
 
 	public void setJobs(List<Job> jobs) {
 		this.jobs = jobs;
+		if (jobs != null) {
+			jobs.forEach(job -> job.setExecutionTimeInSeconds());
+		}
 	}
 
 }

558148a30c161d4683961b1e4286241c7d678e8b
Pooshan <pooshan.singh@talentica.com>
2016-05-02 16:59:19 +0530

    Bug fixes : Script change in sharding.

--- utility/scripts/setup-sharding.sh
+++ utility/scripts/setup-sharding.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 jobuuid=$1
-echo "uuid:"$jobuuid
-sharding_node_ip=`cat ../$1/master_ip_file`
-scp ~/.ssh/id_rsa.pub root@$sharding_node_ip:~/.ssh
-scp ~/.ssh/id_rsa root@$sharding_node_ip:~/.ssh
-sh copy-file-to-sharding.sh start-kazoo-server.sh $jobuuid
+sharding_node_ip=`cat ../$jobuuid/master_ip_file`
 sh shut-down-sharding.sh $jobuuid
 sh cleanup-sharding.sh $jobuuid
 echo 'Copying new build'
 sh copy-file-to-sharding.sh ../lib/sharding*.jar $jobuuid
 echo 'Copying common configuration file'
+echo 'Copying data publishers node configuration file'
+sh copy-file-to-sharding.sh data_publisher_nodes_config.txt $jobuuid
 echo 'Copying nodes'' password file'
-#sh copy-file-to-sharding.sh ../$jobuuid/node_pwd_file.txt $jobuuid
 echo 'Copying shard file copy utility'
 sh copy-file-to-sharding.sh copy-shard-files-to-all-nodes.sh $jobuuid
+sh copy-file-to-sharding.sh start-kazoo-server.sh $jobuuid
+scp ~/.ssh/id_rsa.pub root@$sharding_node_ip:~/.ssh
+scp ~/.ssh/id_rsa root@$sharding_node_ip:~/.ssh

5b80b1136852c0dfce97a00f0f91f193a35fc89b
Nitin Kasat <nitin.kasat@talentica.com>
2016-05-03 16:55:38 +0530

    1. Fix for database getting disconnected after some idle time resulting in not able to login. 2. Added tooltips for time taken. 3. Showing download link for only 24 hours after job finished its execution. 4. Added a single job matrix sample.

---
+++ test-hungry-hippos/src/main/java/com/talentica/hungryHippos/test/sum/SumJobMatrixSingleJob.java
@@ -0,0 +1,23 @@
+package com.talentica.hungryHippos.test.sum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.talentica.hungryHippos.client.job.Job;
+import com.talentica.hungryHippos.client.job.JobMatrix;
+
+public class SumJobMatrixSingleJob implements JobMatrix {
+
+	@Override
+	public List<Job> getListOfJobsToExecute() {
+		List<Job> jobList = new ArrayList<>();
+		jobList.add(new SumJob(new int[] { 0 }, 0, 3));
+		return jobList;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new SumJobMatrixSingleJob().getListOfJobsToExecute());
+		System.out.println(new SumJobMatrixSingleJob().getListOfJobsToExecute().size());
+	}
+
+}

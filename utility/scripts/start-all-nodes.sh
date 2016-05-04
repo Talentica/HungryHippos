e0cfe1268066a74b5b96d6b48e96be86ef23a36a
Pooshan <pooshan.singh@talentica.com>
2016-05-04 14:38:43 +0530

    script logging for out file.

--- utility/scripts/start-all-nodes.sh
+++ utility/scripts/start-all-nodes.sh
@@ -7,5 +7,5 @@ sh remove-ssh-keygen.sh $jobUuid
 for node in `cat ../$jobUuid/node_ips_list.txt`
 do
 echo "Starting HungryHippos node $node"
-ssh -o StrictHostKeyChecking=no root@$node "sh -c 'cd hungryhippos/node;java -cp node.jar:test-jobs.jar com.talentica.hungryHippos.node.DataReceiver $jobUuid > ./system.out 2>./system.err &'"
+ssh -o StrictHostKeyChecking=no root@$node "sh -c 'cd hungryhippos/node;java -cp node.jar:test-jobs.jar com.talentica.hungryHippos.node.DataReceiver $jobUuid > ./system_data_receiver.out 2>./system_data_receiver.err &'"
 done

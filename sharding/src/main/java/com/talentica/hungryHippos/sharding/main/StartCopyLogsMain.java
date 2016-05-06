fbe906cb8d46f9c1768f5850d8484734ba8ef797
Pooshan <pooshan.singh@talentica.com>
2016-05-06 14:49:59 +0530

    Implementation to copy the all logs success/failure to nginx servers.

--- sharding/src/main/java/com/talentica/hungryHippos/sharding/main/StartCopyLogsMain.java
+++ sharding/src/main/java/com/talentica/hungryHippos/sharding/main/StartCopyLogsMain.java
@@ -3,40 +3,43 @@
  */
 package com.talentica.hungryHippos.sharding.main;
 
+import java.nio.file.Paths;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.talentica.hungryHippos.coordination.utility.CommonUtil;
 import com.talentica.hungryHippos.coordination.utility.Property;
 import com.talentica.hungryHippos.coordination.utility.Property.PROPERTIES_NAMESPACE;
+import com.talentica.hungryHippos.utility.PathUtil;
 
 /**
  * @author PooshanS
  *
  */
-public class StartKazooScriptMain {
+public class StartCopyLogsMain {
 
 	private static final Logger LOGGER = LoggerFactory
-			.getLogger(StartKazooScriptMain.class);
+			.getLogger(StartCopyLogsMain.class);
 	private static String jobUUId;
 
 	public static void main(String[] args) {
 		jobUUId = args[0];
 		CommonUtil.loadDefaultPath(jobUUId);
 		Property.initialize(PROPERTIES_NAMESPACE.NODE);
-		callCopyScriptToRunKazoo();
+		callCopyLogsScript();
 	}
-	
-	private static void callCopyScriptToRunKazoo() {
-		LOGGER.info("Calling script file to start kazoo server");
+
+	private static void callCopyLogsScript() {
+		LOGGER.info("Calling script file to start the logs file");
 		String jobuuid = Property.getProperties().getProperty("job.uuid");
-		String webserverIp = Property.getProperties().getProperty(
-				"common.webserver.ip");
-		String[] strArr = new String[] { "/bin/sh", "start-kazoo-server.sh",
-				jobuuid, webserverIp };
+		String pythonScriptPath = Paths
+				.get("/root/hungryhippos/scripts/python_scripts")
+				.toAbsolutePath().toString()
+				+ PathUtil.FORWARD_SLASH;
+		String[] strArr = new String[] { "/usr/bin/python",
+				pythonScriptPath + "copy-all-logs-to-nginx.py", jobuuid, "&" };
 		CommonUtil.executeScriptCommand(strArr);
 		LOGGER.info("Done.");
-
 	}
-
 }

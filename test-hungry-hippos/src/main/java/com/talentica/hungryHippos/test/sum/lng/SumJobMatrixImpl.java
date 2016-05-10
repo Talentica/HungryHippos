f603eace68ce727396c9eeb795e434c648e4581b
Nitin Kasat <nitin.kasat@talentica.com>
2016-05-10 19:38:11 +0530

    Adding long data type's sum job matrix.

---
+++ test-hungry-hippos/src/main/java/com/talentica/hungryHippos/test/sum/lng/SumJobMatrixImpl.java
@@ -0,0 +1,23 @@
+package com.talentica.hungryHippos.test.sum.lng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.talentica.hungryHippos.client.job.Job;
+import com.talentica.hungryHippos.client.job.JobMatrix;
+
+public class SumJobMatrixImpl implements JobMatrix {
+
+	@Override
+	public List<Job> getListOfJobsToExecute() {
+		List<Job> jobList = new ArrayList<>();
+		jobList.add(new SumJob(new int[] { 0 }, 0, 1));
+		return jobList;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new SumJobMatrixImpl().getListOfJobsToExecute());
+		System.out.println(new SumJobMatrixImpl().getListOfJobsToExecute().size());
+	}
+
+}

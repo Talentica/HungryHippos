c3e00c1dd9ea4328a7c0fd7fdab2515f3f959e61
Nitin Kasat <nitin.kasat@talentica.com>
2016-05-11 18:58:02 +0530

    Issue with removing SSH key on master node.

--- utility/scripts/remove-ssh-keygen.sh
+++ utility/scripts/remove-ssh-keygen.sh
@@ -2,7 +2,7 @@
 jobUuid=$1
 cat ../$jobUuid/serverConfigFile.properties|awk -F":" '{print $2}' > ../$jobUuid/node_ips_list_for_keygen.txt
 cat ../$jobUuid/output_ip_file >> ../$jobUuid/node_ips_list_for_keygen.txt
-cat ../$jobUuid/master_ip-file >> ../$jobUuid/node_ips_list_for_keygen.txt
+cat ../$jobUuid/master_ip_file >> ../$jobUuid/node_ips_list_for_keygen.txt
 for node in `cat ../$jobUuid/node_ips_list_for_keygen.txt`
 do
    echo "Removing ssh keygen for $node"

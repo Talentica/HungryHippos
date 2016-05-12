6621d57786634843c46749b781fdec7639bf98b1
Nitin Kasat <nitin.kasat@talentica.com>
2016-05-12 12:16:03 +0530

    Updating FAQs for Javadocs link and upload files URLs.

--- client-api/src/main/java/com/talentica/hungryHippos/client/job/JobMatrix.java
+++ client-api/src/main/java/com/talentica/hungryHippos/client/job/JobMatrix.java
@@ -2,6 +2,12 @@ package com.talentica.hungryHippos.client.job;
 
 import java.util.List;
 
+/**
+ * Job matrix is a collection of jobs to be executed in one run.
+ * 
+ * @author nitink
+ *
+ */
 public interface JobMatrix {
 
 	public List<Job> getListOfJobsToExecute();
--- hungryhippos-tester-web/src/main/resources/static/secure/faqController.js
+++ hungryhippos-tester-web/src/main/resources/static/secure/faqController.js
@@ -3,4 +3,5 @@
 app.controller('FaqCtrl',function ($scope) {
 	$scope.client_jar_location="http://192.241.248.197/client/client-api.zip";
 	$scope.test_job_matrix_jar_location="http://192.241.248.197/sample/test-jobs.zip";
+	$scope.client_api_documentation="http://192.241.248.197/client/docs/index.html";
 });

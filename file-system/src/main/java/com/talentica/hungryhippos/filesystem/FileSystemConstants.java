35cf202003102142e32fd6288dc80b3162c081e0
RajkishoreH <rajkishore.hembram@talentica.com>
2016-07-11 15:14:00 +0530

    Changes done for having filesystem configuration in XML

--- file-system/src/main/java/com/talentica/hungryhippos/filesystem/FileSystemConstants.java
+++ file-system/src/main/java/com/talentica/hungryhippos/filesystem/FileSystemConstants.java
@@ -11,16 +11,10 @@ public interface FileSystemConstants {
     String DATA_TRANSFER_COMPLETED = "DATA_TRANSFER_COMPLETED";
     String FILE_PATHS_DELIMITER = ",";
     String DOWNLOAD_FILE_PREFIX = "part-";
+    String CONFIGURATION_FILE = "filesystem-config";
 
     //constants for file-system configuration
-    String HHROOT = "file.system.root.directory";
-    String DATA_FILE_PREFIX = "file.system.data.file.prefix";
     String ROOT_NODE = "zookeeper.file.system";
-    String SERVER_PORT = "file.system.server.port";
-    String MAX_CLIENT_REQUESTS= "file.system.max.client.requests";
-    String MAX_QUERY_ATTEMPTS="file.system.max.query.attempts";
-    String QUERY_RETRY_INTERVAL = "file.system.query.retry.interval";
-    String FILE_STREAM_BUFFER_SIZE = "file.system.file.stream.buffer.size";
 
 }
 
--- file-system/src/main/java/com/talentica/hungryhippos/filesystem/context/FileSystemContext.java
+++ file-system/src/main/java/com/talentica/hungryhippos/filesystem/context/FileSystemContext.java
@@ -1,23 +1,63 @@
 package com.talentica.hungryhippos.filesystem.context;
 
-import com.talentica.hungryHippos.coordination.property.Property;
-import com.talentica.hungryhippos.filesystem.property.FileSystemProperty;
+import com.talentica.hungryHippos.coordination.NodesManager;
+import com.talentica.hungryHippos.coordination.domain.NodesManagerContext;
+import com.talentica.hungryHippos.coordination.domain.ZKNodeFile;
+import com.talentica.hungryHippos.utility.jaxb.JaxbUtil;
+import com.talentica.hungryhippos.config.filesystem.FileSystemConfig;
+import com.talentica.hungryhippos.filesystem.FileSystemConstants;
+import org.apache.zookeeper.KeeperException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
 /**
+ * This class is for handling the filesystem configuration
  * Created by rajkishoreh on 4/7/16.
  */
 public class FileSystemContext {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FileSystemContext.class);
 
-    private static Property<FileSystemProperty> property;
+    public static FileSystemConfig fileSystemConfig;
 
-    public static Property<FileSystemProperty> getProperty() {
-        if (property == null) {
-            property = new FileSystemProperty("file-system.properties");
+    /**
+     * This method uploads the filesystem configuration file in zookeeper
+     * @param fileSystemConfigFile
+     * @throws IOException
+     * @throws JAXBException
+     * @throws InterruptedException
+     */
+    public static void uploadFileSystemConfig(String fileSystemConfigFile)
+            throws IOException, JAXBException, InterruptedException {
+        LOGGER.info("Updating filesystem configuration on zookeeper");
+        ZKNodeFile serverConfigFile = new ZKNodeFile(FileSystemConstants.CONFIGURATION_FILE, fileSystemConfigFile);
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        NodesManagerContext.getNodesManagerInstance().saveConfigFileToZNode(serverConfigFile, countDownLatch);
+        countDownLatch.await();
+    }
+
+    /**
+     * This method gets the filesystem configuration object from zookeeper filesystem configuration file
+     * @return
+     * @throws IOException
+     * @throws JAXBException
+     * @throws InterruptedException
+     * @throws ClassNotFoundException
+     * @throws KeeperException
+     */
+    public static FileSystemConfig getFileSystemConfig() throws IOException, JAXBException, InterruptedException, ClassNotFoundException, KeeperException {
+        if(fileSystemConfig==null){
+            NodesManager manager = NodesManagerContext.getNodesManagerInstance();
+            ZKNodeFile fileSystemConfigurationFile = (ZKNodeFile) manager
+                    .getConfigFileFromZNode(FileSystemConstants.CONFIGURATION_FILE);
+            fileSystemConfig = JaxbUtil.unmarshal((String) fileSystemConfigurationFile.getObj(),
+                    FileSystemConfig.class);
         }
-        return property;
+
+        return fileSystemConfig;
     }
 }

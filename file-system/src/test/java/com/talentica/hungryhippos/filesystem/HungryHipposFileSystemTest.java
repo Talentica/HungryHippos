a35f415d310ce4f306debe7d347e8901da2cbf10
sudarshans <sudarshan.suresh@talentica.com>
2016-07-20 16:11:46 +0530

    Added few more code and tested

--- file-system/src/test/java/com/talentica/hungryhippos/filesystem/HungryHipposFileSystemTest.java
+++ file-system/src/test/java/com/talentica/hungryhippos/filesystem/HungryHipposFileSystemTest.java
@@ -1,48 +1,60 @@
 package com.talentica.hungryhippos.filesystem;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.concurrent.CountDownLatch;
 
-import org.junit.Ignore;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
-import org.powermock.modules.junit4.PowerMockRunner;
+
 import com.talentica.hungryHippos.coordination.NodesManager;
 import com.talentica.hungryHippos.coordination.context.CoordinationApplicationContext;
 import com.talentica.hungryHippos.coordination.domain.NodesManagerContext;
 import com.talentica.hungryHippos.coordination.property.Property;
 import com.talentica.hungryHippos.coordination.property.ZkProperty;
 
-@Ignore
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({NodesManagerContext.class, CoordinationApplicationContext.class})
-@SuppressStaticInitializationFor({
-    "com.talentica.hungryHippos.coordination.domain.NodesManagerContext"})
+
+/*
+ * @RunWith(PowerMockRunner.class)
+ * 
+ * @PrepareForTest({NodesManagerContext.class, CoordinationApplicationContext.class})
+ * 
+ * @SuppressStaticInitializationFor({
+ * "com.talentica.hungryHippos.coordination.domain.NodesManagerContext"})
+ */
 public class HungryHipposFileSystemTest {
 
-  private HungryHipposFileSystem system = HungryHipposFileSystem.getInstance();
+  private HungryHipposFileSystem hhfs = null;
 
   private NodesManager nodesManager;
   private Property<ZkProperty> zkProperty;
 
+  @Before
   public void setUp() {
-    PowerMockito.mockStatic(NodesManagerContext.class);
+  /*  PowerMockito.mockStatic(NodesManagerContext.class);
     PowerMockito.mockStatic(CoordinationApplicationContext.class);
 
     nodesManager = Mockito.mock(NodesManager.class);
-    zkProperty = Mockito.mock(Property.class);
+    zkProperty = Mockito.mock(Property.class);*/
+    hhfs = HungryHipposFileSystem.getInstance();
   }
 
+  @After
+  public void tearDown(){
+    hhfs = null;
+  }
+  
+  
   @Test
   public void testCreateZnode() {
     String testFolder = "test";
-    String path = system.createZnode(testFolder);
+    String path = hhfs.createZnode(testFolder);
     assertNotNull(path);
   }
 
@@ -50,7 +62,7 @@ public class HungryHipposFileSystemTest {
   public void testCreateZnodeArg2() {
     String testFolder = "test1";
     CountDownLatch signal = new CountDownLatch(1);
-    String path = system.createZnode(testFolder, signal);
+    String path = hhfs.createZnode(testFolder, signal);
     try {
       signal.await();
     } catch (InterruptedException e) {
@@ -62,31 +74,31 @@ public class HungryHipposFileSystemTest {
   @Test
   public void testCreateZnodeArg3() {
     String testFolder = "test2";
-    String path = system.createZnode(testFolder, "testing");
+    String path = hhfs.createZnode(testFolder, "testing");
     assertNotNull(path);
   }
 
   @Test
   public void testCheckZnodeExists() {
-    boolean flag = system.checkZnodeExists("test2");
+    boolean flag = hhfs.checkZnodeExists("test2");
     assertTrue(flag);
   }
 
   @Test
   public void testCheckZnodeExistsNegative() {
-    boolean flag = system.checkZnodeExists("test3");
+    boolean flag = hhfs.checkZnodeExists("test3");
     assertFalse(flag);
   }
 
   @Test
   public void testSetData() {
-    system.setData("test1", "data Passed");
+    hhfs.setData("test1", "data Passed");
 
   }
 
   @Test
   public void testGetData() {
-    String s = system.getData("test1");
+    String s = hhfs.getData("test1");
     assertNotNull(s);
   }
 
@@ -101,19 +113,20 @@ public class HungryHipposFileSystemTest {
       String nodeIp = "localhost";
       String fileNodeZKPath = fileSystemRootNodeZKPath + File.separator + fileZKNode;
       String nodeIpZKPath = fileNodeZKPath + File.separator + nodeIp;
+      /*
+       * PowerMockito.when(NodesManagerContext.getNodesManagerInstance()).thenReturn(nodesManager);
+       * PowerMockito.when(CoordinationApplicationContext.getZkProperty()).thenReturn(zkProperty);
+       * 
+       * Mockito.when(nodesManager.checkNodeExists(fileNodeZKPath)).thenReturn(false);
+       * Mockito.when(nodesManager.checkNodeExists(nodeIpZKPath)).thenReturn(false);
+       * Mockito.when(nodesManager.getObjectFromZKNode(fileNodeZKPath)).thenReturn("0");
+       * Mockito.doNothing().when(nodesManager).createPersistentNode(Mockito.anyString(), new
+       * CountDownLatch(1), Mockito.eq(Mockito.any()));
+       * Mockito.when(zkProperty.getValueByKey(FileSystemConstants.ROOT_NODE))
+       * .thenReturn(fileSystemRootNodeZKPath);
+       */
 
-      PowerMockito.when(NodesManagerContext.getNodesManagerInstance()).thenReturn(nodesManager);
-      PowerMockito.when(CoordinationApplicationContext.getZkProperty()).thenReturn(zkProperty);
-
-      Mockito.when(nodesManager.checkNodeExists(fileNodeZKPath)).thenReturn(false);
-      Mockito.when(nodesManager.checkNodeExists(nodeIpZKPath)).thenReturn(false);
-      Mockito.when(nodesManager.getObjectFromZKNode(fileNodeZKPath)).thenReturn("0");
-      Mockito.doNothing().when(nodesManager).createPersistentNode(Mockito.anyString(),
-          new CountDownLatch(1), Mockito.eq(Mockito.any()));
-      Mockito.when(zkProperty.getValueByKey(FileSystemConstants.ROOT_NODE))
-          .thenReturn(fileSystemRootNodeZKPath);
-
-      system.updateFSBlockMetaData(fileZKNode, nodeIp, dataFileZKNode, datafileSize);
+      hhfs.updateFSBlockMetaData(fileZKNode, nodeIp, dataFileZKNode, datafileSize);
 
       assertTrue(true);
     } catch (Exception e) {

2c94b96f27752e3b3ac5d1623f7cf008a0aec174
Nitin Kasat <nitin.kasat@talentica.com>
2016-08-04 20:07:11 +0530

    http://172.19.101.166:8080/browse/HH-102: Add an ability to copy sharding table to any one of the nodes from client. {r:debasishc}

---
+++ utility/src/test/integration/java/com/talentica/hungryHippos/utility/SecureShellExecutorIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.talentica.hungryHippos.utility;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SecureShellExecutorIntegrationTest {
+
+  private SecureShellExecutor secureShellExecutor = null;
+
+  @Before
+  public void setup() {
+    secureShellExecutor = new SecureShellExecutor("107.170.3.50", "root", "~/.ssh/id_rsa");
+  }
+
+  @Test
+  public void testExecute() {
+    List<String> output = secureShellExecutor.execute("echo hello world");
+    Assert.assertNotNull(output);
+    Assert.assertEquals(1, output.size());
+    Assert.assertEquals("hello world", output.get(0));
+  }
+
+  @Test
+  public void testExecuteForRunningJavaCommand() {
+    secureShellExecutor = new SecureShellExecutor("localhost", "nitink", "~/.ssh/id_rsa");
+    List<String> output = secureShellExecutor.execute(
+        "java -cp git/HungryHippos/installation/lib/test-jobs.jar com.talentica.hungryHippos.test.sum.SumJobMatrixImpl");
+    Assert.assertNotNull(output);
+    Assert.assertEquals(28, output.size());
+    Assert.assertEquals("26", output.get(27));
+  }
+
+}

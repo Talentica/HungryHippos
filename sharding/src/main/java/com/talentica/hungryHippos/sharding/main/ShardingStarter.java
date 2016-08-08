0c1f828ce515a06ec22eb3960f9a68d842bd031c
Nitin Kasat <nitin.kasat@talentica.com>
2016-08-08 19:43:55 +0530

    Fixing compile time issues after merging.

--- sharding/src/main/java/com/talentica/hungryHippos/sharding/main/ShardingStarter.java
+++ sharding/src/main/java/com/talentica/hungryHippos/sharding/main/ShardingStarter.java
@@ -3,10 +3,7 @@ package com.talentica.hungryHippos.sharding.main;
 import java.io.File;
 import java.io.IOException;
 
-import com.talentica.hungryHippos.coordination.ZkUtils;
-import com.talentica.hungryHippos.utility.FileSystemConstants;
 import org.apache.commons.io.FileUtils;
-import org.apache.zookeeper.KeeperException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,17 +12,16 @@ import com.talentica.hungryHippos.client.domain.DataDescription;
 import com.talentica.hungryHippos.coordination.NodesManager;
 import com.talentica.hungryHippos.coordination.context.CoordinationApplicationContext;
 import com.talentica.hungryHippos.coordination.domain.NodesManagerContext;
+import com.talentica.hungryHippos.coordination.domain.ZKNodeFile;
 import com.talentica.hungryHippos.coordination.utility.marshaling.Reader;
 import com.talentica.hungryHippos.sharding.Sharding;
 import com.talentica.hungryHippos.sharding.context.ShardingApplicationContext;
 import com.talentica.hungryHippos.sharding.util.ShardingTableCopier;
 import com.talentica.hungryHippos.utility.jaxb.JaxbUtil;
 import com.talentica.hungryhippos.config.cluster.ClusterConfig;
+import com.talentica.hungryhippos.config.coordination.CoordinationConfig;
 import com.talentica.hungryhippos.config.sharding.Output;
 import com.talentica.hungryhippos.config.sharding.ShardingClientConfig;
-import com.talentica.hungryhippos.config.sharding.ShardingServerConfig;
-
-import javax.xml.bind.JAXBException;
 
 public class ShardingStarter {
 
@@ -35,79 +31,35 @@ public class ShardingStarter {
   private static final Logger LOGGER = LoggerFactory.getLogger(ShardingStarter.class);
 
   public static void main(String[] args) {
-    LOGGER.info("SHARDING PROCESS STARTED");
-    boolean isFileCreated = false;
-    String shardingTablePathOnZk = null;
     try {
+      LOGGER.info("SHARDING STARTED");
+      long startTime = System.currentTimeMillis();
       validateArguments(args);
       String shardingClientConfigFilePath = args[1];
-      NodesManager manager = NodesManagerContext.getNodesManagerInstance(clientConfigFilePath);
-
-      ShardingClientConfig shardingClientConfig = JaxbUtil.unmarshalFromFile(shardingClientConfigFilePath, ShardingClientConfig.class);
-
+      ShardingClientConfig shardingClientConfig =
+          JaxbUtil.unmarshalFromFile(shardingClientConfigFilePath, ShardingClientConfig.class);
       String shardingServerConfigFilePath = args[2];
-      if (distributedFilePath == null || "".equals(distributedFilePath)) {
-        throw new RuntimeException("Distributed File path cannot be empty");
-      }
-      shardingTablePathOnZk = ShardingApplicationContext.getShardingConfigFilePathOnZk(distributedFilePath);
-      boolean fileAlreadyExists = ZkUtils.checkIfNodeExists(shardingTablePathOnZk);
-      if (fileAlreadyExists) {
-        throw new RuntimeException(shardingTablePathOnZk + " already exists");
-      }
-      ZkUtils.createFileNode(shardingTablePathOnZk);
-      isFileCreated = true;
-
-      ShardingServerConfig shardingServerConfig = JaxbUtil.unmarshalFromFile(shardingServerConfigFilePath, ShardingServerConfig.class);
-      ShardingApplicationContext.putShardingClientConfig(distributedFilePath, shardingClientConfig);
-      ShardingApplicationContext.putShardingServerConfig(distributedFilePath, shardingServerConfig);
-
-      doSharding(shardingClientConfig);
-      uploadShardingData(shardingTablePathOnZk, shardingClientConfigFilePath, shardingServerConfigFilePath);
-      LOGGER.info("SHARDING PROCESS COMPLETED SUCCESSFULLY");
-    } catch (Exception exception) {
-      LOGGER.error("Error occurred while sharding.", exception);
-      if (isFileCreated) {
-        ZkUtils.deleteZKNode(shardingTablePathOnZk);
-      }
-    }
-  }
-
-  private static void validateArguments(String[] args) {
-    if (args.length < 1) {
-      throw new RuntimeException("Missing zookeeper xml configuration file path arguments.");
-    }
-  }
-
-  /**
-   * Reads the configurations and starts sharding
-   * @param shardingClientConfig
-   * @throws ClassNotFoundException
-   * @throws IOException
-   * @throws NoSuchMethodException
-   * @throws IllegalAccessException
-   * @throws InvocationTargetException
-   * @throws InstantiationException
-   * @throws InterruptedException
-   * @throws JAXBException
-     * @throws KeeperException
-     */
-  private static void doSharding(ShardingClientConfig shardingClientConfig) throws ClassNotFoundException, IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, InterruptedException, JAXBException, KeeperException {
-    LOGGER.info("SHARDING STARTED");
-    long startTime = System.currentTimeMillis();
-    String distributedFilePath = shardingClientConfig.getInput().getDistributedFilePath();
-    String sampleFilePath = shardingClientConfig.getInput().getSampleFilePath();
-    String dataParserClassName =  shardingClientConfig.getInput().getDataParserConfig().getClassName();
-    DataDescription dataDescription = ShardingApplicationContext.getConfiguredDataDescription(distributedFilePath);
-    DataParser dataParser = (DataParser) Class.forName(dataParserClassName).getConstructor(DataDescription.class)
-            .newInstance(dataDescription);
+      String sampleFilePath = shardingClientConfig.getInput().getSampleFilePath();
+      String dataParserClassName =
+          shardingClientConfig.getInput().getDataParserConfig().getClassName();
+      DataParser dataParser =
+          (DataParser) Class.forName(dataParserClassName).getConstructor(DataDescription.class)
+              .newInstance(ShardingApplicationContext.getConfiguredDataDescription(
+                  shardingClientConfig.getInput().getDistributedFilePath()));
 
       String clientConfigFilePath = args[0];
-    Reader inputReaderForSharding= getInputReaderForSharding(sampleFilePath,dataParser);
-    String outputDirectory = shardingClientConfig.getOutput().getOutputDir();
+      NodesManager manager = NodesManagerContext.getNodesManagerInstance(clientConfigFilePath);
+      CoordinationConfig coordinationConfig =
+          CoordinationApplicationContext.getZkCoordinationConfigCache();
+      manager.initializeZookeeperDefaultConfig(coordinationConfig.getZookeeperDefaultConfig());
 
-    Sharding sharding = new Sharding(clusterConfig);
-    sharding.doSharding(inputReaderForSharding,distributedFilePath);
-    sharding.dumpShardingTableFiles(outputDirectory);
+      ZKNodeFile clusterConfigFile = (ZKNodeFile) manager
+          .getConfigFileFromZNode(CoordinationApplicationContext.CLUSTER_CONFIGURATION);
+      ClusterConfig clusterConfig =
+          JaxbUtil.unmarshal((String) clusterConfigFile.getObj(), ClusterConfig.class);
+      Sharding sharding = new Sharding(clusterConfig);
+      sharding.doSharding(getInputReaderForSharding(sampleFilePath, dataParser),
+          shardingClientConfig.getInput().getDistributedFilePath());
       Output outputConfiguration = shardingClientConfig.getOutput();
       String tempDir = FileUtils.getUserDirectoryPath() + File.separator + "temp" + File.separator
           + "hungryhippos" + File.separator + System.currentTimeMillis();
@@ -117,21 +69,24 @@ public class ShardingStarter {
       ShardingTableCopier shardingTableCopier =
           new ShardingTableCopier(tempDir, shardingClientConfig, outputConfiguration);
       shardingTableCopier.copyToAnyRandomNodeInCluster();
-  }
-   * Uploads Sharding related data to Zookeeper
-   * @param shardingTablePathOnZk
-   * @param shardingClientConfigFilePath
-   * @param shardingServerConfigFilePath
-     */
-  private static void uploadShardingData(String shardingTablePathOnZk, String shardingClientConfigFilePath, String shardingServerConfigFilePath) {
-
-    try {
-      ShardingApplicationContext.uploadShardingConfigOnZk(shardingTablePathOnZk,
-    } catch (IOException | InterruptedException | IllegalAccessException |JAXBException e) {
-      LOGGER.error(e.toString());
-      throw new RuntimeException(e);
+      long endTime = System.currentTimeMillis();
+      LOGGER.info("SHARDING DONE!!");
+      LOGGER.info("It took {} seconds of time to do sharding.", ((endTime - startTime) / 1000));
+    } catch (Exception exception) {
+      LOGGER.error("Error occurred while sharding.", exception);
     }
   }
 
+  private static void validateArguments(String[] args) {
+    if (args.length < 1) {
+      throw new RuntimeException("Missing zookeeper xml configuration file path arguments.");
+    }
+  }
+
+  private static Reader getInputReaderForSharding(String inputFile, DataParser dataParser)
+      throws IOException {
+    return new com.talentica.hungryHippos.coordination.utility.marshaling.FileReader(inputFile,
+        dataParser);
+  }
 
 }

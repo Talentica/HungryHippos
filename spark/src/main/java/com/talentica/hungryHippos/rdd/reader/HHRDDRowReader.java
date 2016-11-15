7041e12045d7c5afd1d47d4acd98294838330875
Sudarshan Suresh <sudarshan.suresh@talentica.com>
2016-11-15 19:17:57 +0530

    "added files that are missed in the previous commit".

---
+++ spark/src/main/java/com/talentica/hungryHippos/rdd/reader/HHRDDRowReader.java
@@ -0,0 +1,92 @@
+package com.talentica.hungryHippos.rdd.reader;
+
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+import com.talentica.hungryHippos.client.domain.DataDescription;
+import com.talentica.hungryHippos.client.domain.DataLocator;
+import com.talentica.hungryHippos.client.domain.MutableCharArrayString;
+
+/**
+ * {@code DynamicMarshal} is used by the system to decode the encrypted file.
+ * 
+ * @author debasishc
+ * @since 1/9/15.
+ */
+public class HHRDDRowReader implements Serializable {
+
+  private static final long serialVersionUID = -5800537222182360030L;
+
+  private DataDescription dataDescription;
+
+  private ByteBuffer source = null;
+
+  public void setByteBuffer(ByteBuffer source) {
+    this.source = source;
+  }
+
+  /**
+   * creates a new instance of DynamicMarshal using the {@value dataDescription}
+   * 
+   * @param dataDescription
+   */
+  public HHRDDRowReader(DataDescription dataDescription) {
+    this.dataDescription = dataDescription;
+  }
+
+  /**
+   * used to read a value from the provided {@value index} and {@value source}.
+   * 
+   * @param index
+   * @param source
+   * @return Object.
+   */
+  public Object readAtColumn(int index) {
+    DataLocator locator = dataDescription.locateField(index);
+    switch (locator.getDataType()) {
+      case BYTE:
+        return source.get(locator.getOffset());
+      case CHAR:
+        return source.getChar(locator.getOffset());
+      case SHORT:
+        return source.getShort(locator.getOffset());
+      case INT:
+        return source.getInt(locator.getOffset());
+      case LONG:
+        return source.getLong(locator.getOffset());
+      case FLOAT:
+        return source.getFloat(locator.getOffset());
+      case DOUBLE:
+        return source.getDouble(locator.getOffset());
+      case STRING:
+        return readValueString(index);
+    }
+    return null;
+  }
+
+  /**
+   * reads MutableCharArrayString value from the given {@value index} from ByteBuffer
+   * {@value source}
+   * 
+   * @param index
+   * @param source
+   * @return {@link MutableCharArrayString}
+   */
+  private MutableCharArrayString readValueString(int index) {
+    DataLocator locator = dataDescription.locateField(index);
+    int offset = locator.getOffset();
+    int size = locator.getSize();
+    MutableCharArrayString charArrayString = new MutableCharArrayString(size);
+    for (int i = offset; i < offset + size; i++) {
+      byte ch = source.get(i);
+      if (ch == 0) {
+        break;
+      }
+      charArrayString.addByte(ch);
+    }
+    return charArrayString;
+  }
+
+
+
+}
---
+++ spark/src/main/java/com/talentica/hungryHippos/rdd/utility/JaxbUtil.java
@@ -0,0 +1,135 @@
+package com.talentica.hungryHippos.rdd.utility;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.MarshalException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import org.eclipse.persistence.jaxb.JAXBContextFactory;
+import org.eclipse.persistence.jaxb.MarshallerProperties;
+import org.eclipse.persistence.jaxb.UnmarshallerProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class JaxbUtil {
+
+  private static Logger LOGGER = LoggerFactory.getLogger(JaxbUtil.class);
+
+  /**
+   * Marshalling supplied object to XML document by JAXB annotations and serializing it to String
+   * 
+   * @param obj object to be marshalled
+   * @return serialized XML document
+   * @throws MarshalException
+   */
+  public static String marshalToXml(Object obj) throws JAXBException {
+    JAXBContext jc = JAXBContextFactory.createContext(new Class[] {obj.getClass()}, null);
+    Marshaller marshaller = jc.createMarshaller();
+    StringWriter stringwriter = new StringWriter();
+    marshaller.marshal(obj, stringwriter);
+    return stringwriter.toString();
+  }
+
+  /**
+   * Marshalling supplied object to XML document by JAXB annotations and serializing it to String
+   * 
+   * @param obj object to be marshalled
+   * @return serialized XML document
+   * @throws MarshalException
+   */
+  public static String marshalToJson(Object obj) {
+    try {
+      JAXBContext jc = JAXBContextFactory.createContext(new Class[] {obj.getClass()}, null);
+      Marshaller marshaller = jc.createMarshaller();
+      StringWriter stringwriter = new StringWriter();
+      marshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/json");
+      marshaller.marshal(obj, stringwriter);
+      return stringwriter.toString();
+    } catch (JAXBException exception) {
+      throw new RuntimeException(exception);
+    }
+  }
+
+  /**
+   * Unmarshalling from XML document by JAXB annotations
+   * 
+   * @param xml xml document serialized as String
+   * @param clazz Class to which shoud be object unmarshalled
+   * @return serialized XML document
+   * @throws JAXBException
+   */
+  @SuppressWarnings("unchecked")
+  public static <T> T unmarshalFromXml(String xml, Class<T> clazz) throws JAXBException {
+    JAXBContext jaxbContext = JAXBContextFactory.createContext(new Class[] {clazz}, null);
+    Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+    return (T) unmarshaller.unmarshal(new StringReader(xml));
+  }
+
+  /**
+   * Unmarshalling from XML document by JAXB annotations
+   * 
+   * @param xml xml document serialized as String
+   * @param clazz Class to which shoud be object unmarshalled
+   * @return serialized XML document
+   * @throws JAXBException
+   */
+  @SuppressWarnings("unchecked")
+  public static <T> T unmarshalFromJson(String json, Class<T> clazz) throws JAXBException {
+    try {
+      JAXBContext jaxbContext = JAXBContextFactory.createContext(new Class[] {clazz}, null);
+      Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+      unmarshaller.setProperty(UnmarshallerProperties.MEDIA_TYPE, "application/json");
+      return (T) unmarshaller.unmarshal(new StringReader(json));
+    } catch (JAXBException exception) {
+      LOGGER.warn("Error while parsing JSON content:-{}", json);
+      throw exception;
+    }
+  }
+
+  /**
+   * Unmarshalling from XML document by JAXB annotations
+   * 
+   * @param xml xml document serialized as String
+   * @param clazz Class to which shoud be object unmarshalled
+   * @return serialized XML document
+   * @throws JAXBException
+   * @throws FileNotFoundException
+   */
+  @SuppressWarnings("unchecked")
+  public static <T> T unmarshalFromFile(String filePath, Class<T> clazz)
+      throws JAXBException, FileNotFoundException {
+    JAXBContext jaxbContext = JAXBContextFactory.createContext(new Class[] {clazz}, null);
+    Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+    if (filePath.endsWith(".json")) {
+      unmarshaller.setProperty(UnmarshallerProperties.MEDIA_TYPE, "application/json");
+    }
+    return (T) unmarshaller.unmarshal(new FileReader(filePath));
+  }
+
+  /**
+   * Unmarshals object from XML or JSON configuration content.
+   * 
+   * @param content
+   * @param clazz
+   * @return
+   * @throws JAXBException
+   * @throws FileNotFoundException
+   */
+  public static <T> T unmarshal(String content, Class<T> clazz)
+      throws JAXBException, FileNotFoundException {
+    T object = null;
+    try {
+      object = unmarshalFromXml(content, clazz);
+    } catch (Exception exception) {
+      object = unmarshalFromJson(content, clazz);
+    }
+    return object;
+  }
+
+}

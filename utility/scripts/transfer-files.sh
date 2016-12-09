bde035f17c864f4fedd23eee5f871d1c3cc5cfb2
RajkishoreH <rajkishore.hembram@talentica.com>
2016-12-09 17:58:55 +0530

    HH-230: bug fix

--- utility/scripts/transfer-files.sh
+++ utility/scripts/transfer-files.sh
@@ -11,8 +11,8 @@ unset ARGS[2]
 unset ARGS[3]
 unset ARGS[4]
 cd $src_folder
-tar -cf $tar_file_name.tar "${ARGS[@]}"
-scp -o StrictHostKeyChecking=no $tar_file_name.tar $ssh_user@$ssh_host:$remote_target_folder"/"
-rm $tar_file_name.tar
-ssh -o StrictHostKeyChecking=no $ssh_user@$ssh_host "tar -xf $remote_target_folder/$tar_file_name.tar --directory $remote_target_folder"
-ssh -o StrictHostKeyChecking=no $ssh_user@$ssh_host "rm $remote_target_folder/$tar_file_name.tar"
+tar -cf $tar_file_name.tar "${ARGS[@]}";
+scp -o StrictHostKeyChecking=no $tar_file_name.tar $ssh_user@$ssh_host:$remote_target_folder"/";
+rm $tar_file_name.tar;
+ssh -o StrictHostKeyChecking=no $ssh_user@$ssh_host "tar -xf $remote_target_folder/$tar_file_name.tar --directory $remote_target_folder";
+ssh -o StrictHostKeyChecking=no $ssh_user@$ssh_host "rm $remote_target_folder/$tar_file_name.tar";

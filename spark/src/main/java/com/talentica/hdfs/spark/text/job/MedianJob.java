7188688d005b5496d52f3928ef7f258910a0112b
sohan <sohan.chauhan@talentica.com>
2017-01-05 12:25:40 +0530

    Refactoring jobs for spark + hadoop env.

--- spark/src/main/java/com/talentica/hdfs/spark/text/job/MedianJob.java
+++ spark/src/main/java/com/talentica/hdfs/spark/text/job/MedianJob.java
@@ -3,6 +3,7 @@ package com.talentica.hdfs.spark.text.job;
 import java.util.Iterator;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -19,18 +20,31 @@ import scala.Tuple2;
 public class MedianJob {
   
   private static Logger LOGGER = LoggerFactory.getLogger(MedianJob.class);
+  private static JavaSparkContext context;
   
-  public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException{
-    SparkContext sc = new SparkContext(args);
-    JavaSparkContext jsc = sc.initContext();
-    JavaRDD<String> rdd = jsc.textFile(sc.getDistrFile());
+  public static void main(String[] args){
+    String masterIp = args[0];
+    String appName = args[1];
+    String inputFile = args[2];
+    String outputDir = args[3];
+    
+    initSparkContext(masterIp,appName);
+    
+    JavaRDD<String> rdd = context.textFile(inputFile);
     for(Job job : getSumJobMatrix().getJobs()){
-      Broadcast<Job> broadcastJob = jsc.broadcast(job);
-      runJob(sc,rdd,broadcastJob);
+      Broadcast<Job> broadcastJob = context.broadcast(job);
+      runJob(rdd,broadcastJob,outputDir);
     }
   }
   
-  public static void runJob(SparkContext sc,JavaRDD<String> rdd,Broadcast<Job> broadcastJob){
+  private static void initSparkContext(String masterIp,String appName){
+    if(context == null){
+      SparkConf conf = new SparkConf().setMaster(masterIp).setAppName(appName);
+      context = new JavaSparkContext(conf);
+    }
+  }
+  
+  public static void runJob(JavaRDD<String> rdd,Broadcast<Job> broadcastJob,String outputDir){
     JavaPairRDD<String, Double> pairRDD = rdd.mapToPair(new PairFunction<String, String, Double>() {
       private static final long serialVersionUID = -1129787304947692082L;
       @Override
@@ -58,12 +72,12 @@ public class MedianJob {
         Double median = descriptiveStatistics.getPercentile(50);
         return new Tuple2<String, Double>(t._1, median);
       }});
-    result.saveAsTextFile(sc.getOutputDir() + broadcastJob.value().getJobId());
+    result.saveAsTextFile(outputDir + broadcastJob.value().getJobId());
     LOGGER.info("Output files are in directory {}",
-        sc.getOutputDir() +  broadcastJob.value().getJobId());
+        outputDir +  broadcastJob.value().getJobId());
   }
   
-  private static JobMatrix getSumJobMatrix() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+  private static JobMatrix getSumJobMatrix(){
     JobMatrix medianJobMatrix = new JobMatrix();
     medianJobMatrix.addJob(new Job(new Integer[] {0,1},6,0));
     return medianJobMatrix;

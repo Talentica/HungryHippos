43e52ef0e13508747ae3c67985d6679c140c3c10
pooshans <pooshans@pooshans>
2017-01-11 16:44:19 +0530

    DescriptiveStatistics median calculator is replaced with our own written median calculator

--- spark/src/main/java/com/talentica/spark/job/executor/MedianJobExecutor.java
+++ spark/src/main/java/com/talentica/spark/job/executor/MedianJobExecutor.java
@@ -1,13 +1,15 @@
 package com.talentica.spark.job.executor;
 
-import com.talentica.hungryHippos.client.domain.FieldTypeArrayDataDescription;
-import com.talentica.hungryHippos.client.domain.MutableCharArrayString;
-import com.talentica.hungryHippos.rdd.HHJavaRDD;
-import com.talentica.hungryHippos.rdd.HHRDD;
-import com.talentica.hungryHippos.rdd.job.Job;
-import com.talentica.hungryHippos.rdd.reader.HHRDDRowReader;
-import com.talentica.hungryHippos.rdd.utility.HHRDDHelper;
-import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import java.io.File;
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -16,76 +18,79 @@ import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.broadcast.Broadcast;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import scala.Tuple2;
 
-import java.io.File;
-import java.io.Serializable;
-import java.nio.ByteBuffer;
-import java.util.*;
-import java.util.Map.Entry;
+import com.talentica.hungryHippos.client.domain.FieldTypeArrayDataDescription;
+import com.talentica.hungryHippos.client.domain.MutableCharArrayString;
+import com.talentica.hungryHippos.rdd.HHJavaRDD;
+import com.talentica.hungryHippos.rdd.HHRDD;
+import com.talentica.hungryHippos.rdd.job.Job;
+import com.talentica.hungryHippos.rdd.reader.HHRDDRowReader;
+import com.talentica.hungryHippos.rdd.utility.HHRDDHelper;
+import com.talentica.hungryHippos.utility.MedianCalculator;
+
+import scala.Tuple2;
 
 /**
  * Created by rajkishoreh on 16/12/16.
  */
 public class MedianJobExecutor implements Serializable {
-    private static final long serialVersionUID = -8292896082222169848L;
-    private static Logger LOGGER = LoggerFactory.getLogger(SumJobExecutor.class);
+	private static final long serialVersionUID = -8292896082222169848L;
+	private static Logger LOGGER = LoggerFactory.getLogger(SumJobExecutor.class);
 
+	public void startMedianJob(HHRDD hipposRDD, Broadcast<FieldTypeArrayDataDescription> descriptionBroadcast,
+			Broadcast<Job> jobBroadcast, String ouputDirectory) {
+		JavaPairRDD<String, Double> pairRDD = hipposRDD.toJavaRDD()
+				.mapToPair(new PairFunction<byte[], String, Double>() {
+					@Override
+					public Tuple2<String, Double> call(byte[] bytes) throws Exception {
+						HHRDDRowReader reader = new HHRDDRowReader(descriptionBroadcast.getValue());
+						ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+						reader.setByteBuffer(byteBuffer);
+						String key = "";
+						for (int index = 0; index < jobBroadcast.value().getDimensions().length; index++) {
+							key = key + ((MutableCharArrayString) reader
+									.readAtColumn(jobBroadcast.value().getDimensions()[index])).toString();
+						}
+						key = key + "|id=" + jobBroadcast.value().getJobId();
+						Double value = (Double) reader.readAtColumn(jobBroadcast.value().getCalculationIndex());
+						return new Tuple2<String, Double>(key, value);
+					}
+				});
 
-    public void startMedianJob(HHRDD hipposRDD, Broadcast<FieldTypeArrayDataDescription> descriptionBroadcast,
-                               Broadcast<Job> jobBroadcast, String ouputDirectory) {
-        JavaPairRDD<String, Double> pairRDD =
-                hipposRDD.toJavaRDD().mapToPair(new PairFunction<byte[], String, Double>() {
-                    @Override
-                    public Tuple2<String, Double> call(byte[] bytes) throws Exception {
-                        HHRDDRowReader reader = new HHRDDRowReader(descriptionBroadcast.getValue());
-                        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
-                        reader.setByteBuffer(byteBuffer);
-                        String key = "";
-                        for (int index = 0; index < jobBroadcast.value().getDimensions().length; index++) {
-                            key = key + ((MutableCharArrayString) reader
-                                    .readAtColumn(jobBroadcast.value().getDimensions()[index])).toString();
-                        }
-                        key = key + "|id=" + jobBroadcast.value().getJobId();
-                        Double value = (Double) reader.readAtColumn(jobBroadcast.value().getCalculationIndex());
-                        return new Tuple2<String, Double>(key, value);
-                    }
-                });
+		JavaRDD<Tuple2<String, Double>> resultRDD = pairRDD
+				.mapPartitions(new FlatMapFunction<Iterator<Tuple2<String, Double>>, Tuple2<String, Double>>() {
+					@Override
+					public Iterator<Tuple2<String, Double>> call(Iterator<Tuple2<String, Double>> t) throws Exception {
 
-        JavaRDD<Tuple2<String, Double>> resultRDD = pairRDD.mapPartitions(new FlatMapFunction<Iterator<Tuple2<String, Double>>, Tuple2<String, Double>>() {
-            @Override
-            public Iterator<Tuple2<String, Double>> call(Iterator<Tuple2<String, Double>> t) throws Exception {
+						List<Tuple2<String, Double>> medianList = new ArrayList<>();
+						Map<String, MedianCalculator<Double>> map = new HashMap<>();
+						while (t.hasNext()) {
+							Tuple2<String, Double> tuple2 = t.next();
+							MedianCalculator<Double> MedianCalculator = map.get(tuple2._1);
+							if (MedianCalculator == null) {
+								MedianCalculator = new MedianCalculator<Double>();
+								map.put(tuple2._1, MedianCalculator);
+							}
+							MedianCalculator.add(tuple2._2);
+						}
 
-                List<Tuple2<String, Double>> medianList = new ArrayList<>();
-                Map<String, DescriptiveStatistics> map = new HashMap<>();
-                while (t.hasNext()) {
-                    Tuple2<String, Double> tuple2 = t.next();
-                    DescriptiveStatistics descriptiveStatistics = map.get(tuple2._1);
-                    if (descriptiveStatistics == null) {
-                        descriptiveStatistics = new DescriptiveStatistics();
-                        map.put(tuple2._1, descriptiveStatistics);
-                    }
-                    descriptiveStatistics.addValue(tuple2._2);
-                }
+						for (Entry<String, MedianCalculator<Double>> entry : map.entrySet()) {
+							Double median = entry.getValue().getMedian();
+							medianList.add(new Tuple2<String, Double>(entry.getKey(), median));
+						}
+						return medianList.iterator();
+					}
+				}, true);
 
-                for (Entry<String, DescriptiveStatistics> entry : map.entrySet()) {
-                    Double median = entry.getValue().getPercentile(50);
-                    medianList.add(new Tuple2<String, Double>(entry.getKey(), median));
-                }
-                return medianList.iterator();
-            }
-        }, true);
+		String outputDistributedPath = ouputDirectory + File.separator + jobBroadcast.value().getJobId();
 
-        String outputDistributedPath = ouputDirectory + File.separator + jobBroadcast.value().getJobId();
+		String outputActualPath = HHRDDHelper.getActualPath(outputDistributedPath);
+		new HHJavaRDD<Tuple2<String, Double>>(resultRDD.rdd(), resultRDD.classTag()).saveAsTextFile(outputActualPath);
+		LOGGER.info("Output files are in directory {}", outputActualPath);
+	}
 
-        String outputActualPath =  HHRDDHelper.getActualPath(outputDistributedPath);
-        new HHJavaRDD<Tuple2<String, Double>>(resultRDD.rdd(),
-                resultRDD.classTag()).saveAsTextFile(outputActualPath);
-        LOGGER.info("Output files are in directory {}", outputActualPath);
-    }
-
-    public void stop(JavaSparkContext context) {
-        context.stop();
-    }
+	public void stop(JavaSparkContext context) {
+		context.stop();
+	}
 
 }

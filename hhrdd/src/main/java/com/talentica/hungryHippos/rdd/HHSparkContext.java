34bfee6e6f4d2676fa144c9cf5a96fcfb623edc1
RajkishoreH <rajkishore.hembram@talentica.com>
2017-03-08 12:35:00 +0530

    HH-286 : Changes done for HHRDDInfo Cache

--- hhrdd/src/main/java/com/talentica/hungryHippos/rdd/HHSparkContext.java
+++ hhrdd/src/main/java/com/talentica/hungryHippos/rdd/HHSparkContext.java
@@ -26,20 +26,28 @@ public class HHSparkContext extends JavaSparkContext {
 
   public JavaRDD<byte[]> binaryRecords(Job job, String hhFilePath)
       throws FileNotFoundException, JAXBException {
+    HHRDDInfo hhrddInfo = getHHRDDInfo(hhFilePath);
+    return new HHRDD(this, hhrddInfo, job.getDimensions(), true).toJavaRDD();
+  }
+
+  private HHRDDInfo getHHRDDInfo(String hhFilePath)
+          throws JAXBException, FileNotFoundException {
     HHRDDInfo hhrddInfo = hhrddInfoCache.get(hhFilePath);
     if (hhrddInfo == null) {
       hhrddInfo = HHRDDHelper.getHhrddInfo(hhFilePath);
       hhrddInfoCache.put(hhFilePath, hhrddInfo);
     }
-    return new HHRDD(this, hhrddInfo, job.getDimensions(), true).toJavaRDD();
+    return hhrddInfo;
   }
 
-  public Broadcast<DataDescription> broadcastFieldDataDescription(String hhFilePath) {
-    return broadcast(hhrddInfoCache.get(hhFilePath).getFieldDataDesc());
+  public Broadcast<DataDescription> broadcastFieldDataDescription(String hhFilePath)
+          throws JAXBException, FileNotFoundException {
+    return broadcast(getHHRDDInfo(hhFilePath).getFieldDataDesc());
   }
 
-  public int[] getShardingIndexes(String hhFilePath) {
-    return hhrddInfoCache.get(hhFilePath).getShardingIndexes();
+  public int[] getShardingIndexes(String hhFilePath)
+          throws JAXBException, FileNotFoundException {
+    return getHHRDDInfo(hhFilePath).getShardingIndexes();
   }
 
   public String getActualPath(String path) {

201e53e8ab55bf42c28d6f9354072e46abc99f47
RajkishoreH <rajkishore.hembram@talentica.com>
2017-05-30 15:19:34 +0530

    Updated chef code to install openjdk 8 instead of oracle jdk 8

--- hhspark_automation/chef/src/cookbooks/HH_java/attributes/java.rb
+++ hhspark_automation/chef/src/cookbooks/HH_java/attributes/java.rb
@@ -1,3 +1,2 @@
 normal['java']['jdk_version'] = '8'
-normal['java']['install_flavor'] = 'oracle'
-normal['java']['oracle']['accept_oracle_download_terms'] = true
+normal['java']['install_flavor'] = 'openjdk'
